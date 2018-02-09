2e3dd351bdfb3b41e6112b38df0b1d997c944057
Runze Jiang <515798163@qq.com>
2018-02-09 15:24:55 +0800

    更新脚本

--- shell/update.sh
+++ shell/update.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
+#:<<!
 
-:<<!
+#假设用户目录下有多个 git 仓库，写脚本实现同时更新在当前目录下的所有git仓库#代码。
+#要求脚本需要接收一个参数 branch ， 当不传 branch 时，仅更新代码；当传 #branch 时，需要切换当分支 branch ，然后再更新代码。
 
-假设用户目录下有多个 git 仓库，写脚本实现同时更新在当前目录下的所有git仓库代码。
-要求脚本需要接收一个参数 branch ， 当不传 branch 时，仅更新代码；当传 branch 时，需要切换当分支 branch ，然后再更新代码。
-
-!
+#!
+#! /bin/bash
+for i in $(ls)
+	do
+		if [ -d $i ]
+		then
+			cd $i
+			read a
+			if [ -n $a ]
+				then
+					git pull
+					cd ..
+				else
+					git checkout $a
+				fi
+		fi
+		done
